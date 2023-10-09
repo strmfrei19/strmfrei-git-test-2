49300210bfac7ae069c425fa2c79e28c44200592
Nabila Syafaat <nabilasyafaat1902@gmail.com>
2023-10-09 09:56:56 +0700

    manambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ullam expedita molestias sunt laudantium iure quaerat perspiciatis! Sunt, inventore earum explicabo et eos, nobis magni voluptate quaerat eum incidunt omnis.</p>
+    <li>dua</li>
 </body>
 </html>
